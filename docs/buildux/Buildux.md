4e06136e3f7c6afc557e53693a9c76b27a129972
Ezequiel López <137637051+elopezwembii@users.noreply.github.com>
2023-07-17 04:02:36 -0300

    Updated Buildux section Updated edit link

---
+++ docs/buildux/Buildux.md
@@ -0,0 +1,5 @@
+# Buildux
+
+What is buildux?
+
+Buildux is a slice builder that allows inject services into reducers and thunks creating a context and consume services from reducers easy. It works with redux-toolkit library.
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -51,7 +51,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+            "https://github.com/Lopez15-Hub/reprovider-docs/tree/master/docs",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
